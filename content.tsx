bfb8e2a3d4b95e61afa305b90adc66b7d4f41b83
Thomas Wade <w1220w@126.com>
2024-04-29 01:06:50 +0800

    feat: xhr changer

--- content.tsx
+++ content.tsx
@@ -1,11 +1,13 @@
 import cssText from "data-text:~style.css"
 import type { PlasmoCSConfig } from "plasmo"
+import { useEffect } from "react"
 
-import { CountButton } from "~features/count-button"
+import { Search } from "~features/search"
 
 export const config: PlasmoCSConfig = {
-  matches: ["https://www.plasmo.com/*"]
-}
+  matches: ["https://www.google.com/maps/*"],
+  world: "MAIN"
+} 
 
 export const getStyle = () => {
   const style = document.createElement("style")
@@ -15,8 +17,8 @@ export const getStyle = () => {
 
 const PlasmoOverlay = () => {
   return (
-    <div className="plasmo-z-50 plasmo-flex plasmo-fixed plasmo-top-32 plasmo-right-8">
-      <CountButton />
+    <div className="plasmo-z-50  plasmo-fixed plasmo-rounded plasmo-top-32 plasmo-right-8 plasmo-bg-white plasmo-p-3 plasmo-pb-1 plasmo-w-96">
+      <Search />
     </div>
   )
 }
---
+++ features/search.tsx
@@ -0,0 +1,21 @@
+import { SearchOutlined } from "@ant-design/icons"
+import { Button } from "antd"
+import { useReducer } from "react"
+
+export const Search = () => {
+  const handleSearch = () => {
+    const input = document.querySelector<HTMLInputElement>("#searchbox input")
+    console.log(input.value)
+    const value = input.value;
+    if (!value) {
+      alert(`Please enter an address or name of a place.`)
+      return;
+    }
+  }
+  return (
+    <div className="plasmo-text-center w-full">
+      <div onClick={handleSearch} className="plasmo-w-full plasmo-border plasmo-border-light-900 plasmo-cursor-pointer plasmo-p-2 plasmo-text-3 plasmo-text-gray-600 plasmo-rounded"><SearchOutlined /> Start Finding Leads</div>
+      <div className="plasmo-text-[10px] plasmo-text-gray-400 plasmo-pt-3">Powered by leads</div>
+    </div>
+  )
+}
