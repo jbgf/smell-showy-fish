293ffd285b64f926030e900a4d95dc2c1916d84f
Thomas Wade <w1220w@126.com>
2024-05-09 09:29:23 +0800

    feat: Scrolled to bottom

--- features/search.tsx
+++ features/search.tsx
@@ -1,15 +1,27 @@
 import { SearchOutlined } from "@ant-design/icons"
 import { sendToBackgroundViaRelay } from "@plasmohq/messaging"
 import { Button } from "antd"
-import { useReducer } from "react"
+import { useEffect, useReducer, useRef, useState } from "react"
 import { SearchTypes } from "~const/enum"
 
 export const Search = () => {
+  /** mock data */
+  const [isSearching, setIsSearching] = useState(true)
+  const isSearchingRef = useRef(isSearching);
+
+  useEffect(() => {
+    isSearchingRef.current = isSearching;
+  }, [isSearching]);
   const scrollToBottom = (element) => {
+
     return new Promise((resolve) => {    
       setTimeout(() => {
+        if (!isSearchingRef.current) {
+          return resolve(false);
+        }
         if (element.scrollTop + element.clientHeight === element.scrollHeight) {
           console.log('Scrolled to bottom');
+          setIsSearching(false)
           return resolve(true)
         } else {
           element.scrollTop = element.scrollHeight;
@@ -48,14 +60,21 @@ export const Search = () => {
       return;
     }
     const searchButton = document.querySelector<HTMLButtonElement>("#searchbox-searchbutton")
-    console.log(searchButton, 'searchButton')
+    // console.log(searchButton, 'searchButton')
     searchButton?.click()
+    setIsSearching(true)
     search()
     
   }
   return (
     <div className="plasmo-text-center w-full">
-      <div onClick={handleSearch} className="plasmo-w-full plasmo-border plasmo-border-light-900 plasmo-cursor-pointer plasmo-p-2 plasmo-text-3 plasmo-text-gray-600 plasmo-rounded"><SearchOutlined /> Start Finding Leads</div>
+      {isSearching 
+      ? <div onClick={handleSearch} className="plasmo-w-full plasmo-border plasmo-border-light-900 plasmo-cursor-pointer plasmo-p-2 plasmo-text-3 plasmo-text-gray-600 plasmo-rounded">
+          <SearchOutlined />
+          Finding leads...
+          
+        </div>  
+      : <div onClick={handleSearch} className="plasmo-w-full plasmo-border plasmo-border-light-900 plasmo-cursor-pointer plasmo-p-2 plasmo-text-3 plasmo-text-gray-600 plasmo-rounded"><SearchOutlined /> Start Finding Leads</div>}
       <div className="plasmo-text-[10px] plasmo-text-gray-400 plasmo-pt-3">Powered by leads</div>
     </div>
   )
